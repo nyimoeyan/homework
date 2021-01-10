d07e00787897f36919f1e4ef24bf47b38abb207f
nyimoeyan <63220597+nyimoeyan@users.noreply.github.com>
2021-01-10 21:46:24 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var marks = 96;
+
+
+if (marks >=0 && marks <= 40){
+    console.log("You Fail The Exam");
+}else if (marks >=41 && marks <=65){
+    console.log("You Pass The Exam");
+}else if (marks >= 66 && marks <= 85){
+    console.log("Credit In Exam");
+}else if (marks >=86 && marks <= 100){
+    console.log("distinction In Exam");
+}else {console.log("Error"); }
+
+
