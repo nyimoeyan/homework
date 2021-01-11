33f4f59bd95eb1eff3335df659ece73108c8c5f6
nyimoeyan <63220597+nyimoeyan@users.noreply.github.com>
2021-01-11 12:24:27 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var marks = 96;
 
 
-if (marks >=0 && marks <= 40){
+if (marks >=0 && marks < 40){
     console.log("You Fail The Exam");
-}else if (marks >=41 && marks <=65){
+}else if (marks >=40 && marks < 65){
     console.log("You Pass The Exam");
-}else if (marks >= 66 && marks <= 85){
+}else if (marks >= 65 && marks < 85){
     console.log("Credit In Exam");
-}else if (marks >=86 && marks <= 100){
+}else if (marks >= 85 && marks <= 100){
     console.log("distinction In Exam");
 }else {console.log("Error"); }
 
